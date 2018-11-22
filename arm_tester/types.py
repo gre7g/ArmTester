276e5883cdbe13abc61af3c3045622ccf2abc001
Gre7g Luterman <greg.luterman@synapse-wireless.com>
2018-11-22 16:16:37 -0600

    Planning some types

--- arm_tester/types.py
+++ arm_tester/types.py
@@ -1,18 +1,79 @@
 import logging
+import struct
 import unicorn.arm_const as arm
 
 LOG = logging.getLogger(__name__)
 
 
-class Unsigned16(object):
+class TypeClass(object):
+    TEMPLATE = None
+
     def __init__(self, name=None):
         self.name = name
+
+    def to_memory(self, value, uc, addr):
+        uc.mem_write(addr, struct.pack(self.TEMPLATE, value))
+
+    def from_memory(self, value, uc, addr):
+        return struct.unpack(self.TEMPLATE, uc.mem_read(addr, struct.calcsize(self.TEMPLATE)))[0]
 
     def decode(self, value):
         return str(value)
 
     def return_value(self, value, program):
         program.uc.reg_write(arm.UC_ARM_REG_R0, value)
+
+
+class Unsigned8(TypeClass):
+    TEMPLATE = "<B"
+
+
+class Unsigned16(TypeClass):
+    TEMPLATE = "<H"
+
+
+class Unsigned32(TypeClass):
+    TEMPLATE = "<L"
+
+
+class Signed8(TypeClass):
+    TEMPLATE = "<b"
+
+
+class Signed16(TypeClass):
+    TEMPLATE = "<h"
+
+
+class Signed32(TypeClass):
+    TEMPLATE = "<l"
+
+
+class ZString(TypeClass):
+    pass
+
+
+class PString(TypeClass):
+    pass
+
+
+class PointerTo(TypeClass):
+    pass
+
+
+class ArrayOf(TypeClass):
+    pass
+
+
+class EnumOf(TypeClass):
+    pass
+
+
+class StructOf(TypeClass):
+    pass
+
+
+class UnionOf(TypeClass):
+    pass
 
 
 class Parameter(object):
